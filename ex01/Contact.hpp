2059ed0b3028e1c141469833346e302170049a4e
Alistair <alkane@student@.42.fr>
2022-06-17 12:45:38 +0200

    ex01 files

---
+++ ex01/Contact.hpp
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alkane <alkane@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/17 12:37:00 by alkane            #+#    #+#             */
+/*   Updated: 2022/06/17 12:37:02 by alkane           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class MyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
+};
