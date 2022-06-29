17758f813b44198a32eaea9c5a4f6c9490c3a85a
Alistair Kane <alkane@c2s18d119.42wolfsburg.de>
2022-06-29 17:39:32 +0200

    fixed ex02

--- C02/ex00/Fixed.cpp
+++ C02/ex00/Fixed.cpp
@@ -1,35 +1,41 @@
 #include "Fixed.hpp"
 
 // Constructors
-Fixed::Fixed()
+Fixed::Fixed(void) //: _value(0)
 {
+	std::cout << "\e[0;33mDefault Constructor called\e[0m" << std::endl;
 	_value = 0;
-	std::cout << "\e[0;33mDefault Constructor called of Fixed\e[0m" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &copy)
 {
-	_value = copy.getRawBits();
-	std::cout << "\e[0;33mCopy Constructor called of Fixed\e[0m" << std::endl;
-}
-
-Fixed::Fixed(void)
-{
-	_value = 0;
-	std::cout << "\e[0;33mFields Constructor called of Fixed\e[0m" << std::endl;
+	std::cout << "\e[0;33mCopy Constructor called\e[0m" << std::endl;
+	*this = copy;
 }
 
 // Destructor
 Fixed::~Fixed()
 {
-	std::cout << "\e[0;31mDestructor called of Fixed\e[0m" << std::endl;
+	std::cout << "\e[0;31mDestructor called\e[0m" << std::endl;
 }
 
 // Operators
 Fixed & Fixed::operator=(const Fixed &copy)
 {
-	_value = copy.getRawBits();
+	std::cout << "\e[0;36mCopy assignment operator called\e[0m" << std::endl;
+	this->_value = copy.getRawBits();
 	return (*this);
 }
 
 // Getters / Setters
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "\e[0;32mgetRawBits member function called\e[0m" << std::endl;
+	return (this->_value);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "\e[0;32msetRawBits member function called\e[0m" << std::endl;
+	_value = raw;
+}
--- C02/ex00/main.cpp
+++ C02/ex00/main.cpp
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alkane <alkane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 14:55:37 by alkane            #+#    #+#             */
-/*   Updated: 2022/06/29 14:58:42 by alkane           ###   ########.fr       */
+/*   Updated: 2022/06/29 17:25:31 by alkane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
-#include <iostream>
 
 int	main(void)
 {
-	Fixed a;
-	Fixed b(a);
-	Fixed c;
-	
+	Fixed	a;
+	Fixed	b(a);
+	Fixed	c;
+
 	c = b;
-	
+
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
