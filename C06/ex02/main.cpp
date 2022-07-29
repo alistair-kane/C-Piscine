34ac5a67afc09db38d439a7e152846aa9404f48c
alkane <alistair@student.42.fr>
2022-07-29 22:32:18 +0200

    CPP06, ex02

--- C06/ex02/main.cpp
+++ C06/ex02/main.cpp
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alkane <alkane@student.42.fr>              +#+  +:+       +#+        */
+/*   By: alistair <alistair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 14:10:44 by alkane            #+#    #+#             */
-/*   Updated: 2022/07/27 18:05:25 by alkane           ###   ########.fr       */
+/*   Updated: 2022/07/29 22:31:19 by alistair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ctime>
+#include <cstdlib>
+#include <exception>
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
@@ -18,35 +20,62 @@
 
 Base* generate(void)
 {
-	Base *pointer = NULL;
-
 	int r_val = std::rand() % 3;
+
 	if (r_val == 0)
-	{
-		std::cout << "A" << std::endl;
-		pointer = new A();
-	}
-	else if (r_val == 1)
-	{
-		std::cout << "B" << std::endl;
-		pointer = new B();
-	}
+		return new A();
+	if (r_val == 1)
+		return new B();
 	else
+		return new C();
+}
+
+void	identify(Base* p)
+{
+	A *a = dynamic_cast<A *>(p);
+	if (a != NULL)
+		std::cout << "Object (pointer) is actally of type A" << std::endl;
+	B *b = dynamic_cast<B *>(p);
+	if (b != NULL)
+		std::cout << "Object (pointer) is actally of type B" << std::endl;
+	C *c = dynamic_cast<C *>(p);
+	if (c != NULL)
+		std::cout << "Object (pointer) is actally of type C" << std::endl;
+}
+
+void	identify(Base& p)
+{
+	try 
 	{
-		std::cout << "C" << std::endl;
-		pointer = new C();
+		(void) dynamic_cast<A &>(p);
+		std::cout << "Object (reference) is actally of type A" << std::endl;
 	}
-	return pointer;
+	catch (std::exception &bc)
+	{}
+	try 
+	{
+		(void) dynamic_cast<B &>(p);
+		std::cout << "Object (reference) is actally of type B" << std::endl;
+	}
+	catch (std::exception &bc)
+	{}
+	try 
+	{
+		(void) dynamic_cast<C &>(p);
+		std::cout << "Object (reference) is actally of type C" << std::endl;
+	}
+	catch (std::exception &bc)
+	{}
 }
 
 int	main(void)
 {
 	std::srand(std::time(0));
-	
-	for (int i = 0; i < 20; i++)
-	{
-		Base *base = generate();
-		delete base;
-	}
+
+	Base *base = generate();
+
+	identify(base);
+	identify(*base);
+	delete base;
 	return 0;
 }
