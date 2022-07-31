e7fdaa6e3a3df96a1236b88b69337daf9c8c25c8
Alistair Kane <alkane@c2s18d119.42wolfsburg.de>
2022-07-31 15:39:36 +0200

    added CPP07+

--- C07/ex00/main.cpp
+++ C07/ex00/main.cpp
@@ -6,16 +6,16 @@
 /*   By: alkane <alkane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 15:25:19 by alkane            #+#    #+#             */
-/*   Updated: 2022/07/31 15:30:20 by alkane           ###   ########.fr       */
+/*   Updated: 2022/07/31 15:32:10 by alkane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main( void )
+int main(void)
 {
 	int a = 2;
 	int b = 3;
 
-	::swap( a, b );
+	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
@@ -27,4 +27,6 @@ int main( void )
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	
+	return 0;
 }
