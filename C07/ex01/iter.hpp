df8fa877ae40a5ff4cb6525a92d47a33e2311f9d
Alistair <alkane@student@.42.fr>
2022-09-08 17:10:46 +0200

    c07 updates

--- C07/ex01/iter.hpp
+++ C07/ex01/iter.hpp
@@ -5,12 +5,18 @@
 # include <string>
 
 template <typename T>
-void iter(T *array, size_t len, void (*func)(T &element))
+void	iter(T *array, size_t len, void (*func)(T &element))
 {
 	for (size_t i = 0; i < len; i++)
 	{
 		(*func)(array[i]);
 	}
+}
+
+template <typename T>
+void	print_element(T &element)
+{
+	std::cout << element << std::endl;
 }
 
 #endif
--- C07/ex01/main.cpp
+++ C07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: alkane <alkane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 15:25:19 by alkane            #+#    #+#             */
-/*   Updated: 2022/09/07 19:17:43 by alkane           ###   ########.fr       */
+/*   Updated: 2022/09/08 14:14:52 by alkane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,14 @@
 int main(void)
 {
 	int int_array[] = {7, 6, 4, 5, 4};
-	float float_array[] = {4.5, 3.3, 5.3, 600.3};
+	float float_array[] = {4.5, 3.3, 5.3, 600.3, 49.2};
 	std::string string_array[] = {"test", "words", "in", "my", "array"};
+	
+	iter(int_array, 5, print_element);
+	std::cout << std::endl;
+	iter(float_array, 5, print_element);
+	std::cout << std::endl;
+	iter(string_array, 5, print_element);
 	
 	return 0;
 }
