1c13d3353487f9edcaacaed333607ebbbd763710
Alistair Kane <alkane@c2s5d13.42wolfsburg.de>
2023-05-08 19:11:40 +0200

    thanks chatgpt

--- C09/ex02/PmergeMe.cpp
+++ C09/ex02/PmergeMe.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alistair <alistair@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alkane <alkane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 14:36:18 by alistair          #+#    #+#             */
-/*   Updated: 2023/05/08 14:55:39 by alistair         ###   ########.fr       */
+/*   Updated: 2023/05/08 19:09:41 by alkane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,126 +68,133 @@ std::list<int>	PmergeMe::copy_list(int start_idx, int end_idx)
 	return (copied);
 }
 
-// void	PmergeMe::merge_list(int start_idx, int mid_idx, int end_idx)
-// {
-// 	int	pos1 = mid_idx - start_idx + 1;
-// 	int pos2 = end_idx - mid_idx;
-// 	std::list<int> left_list = copy_list(start_idx, mid_idx + 1);
-// 	std::list<int> right_list = copy_list(mid_idx + 1, end_idx + 1);
-// 	int ridx = 0;
-// 	int lidx = 0;
-
-// 	std::list<int>::iterator	it = _list.begin();
-// 	for (int i = 0; i < start_idx; i++)
-// 		it++;
-
-// 	for (int i = start_idx; i < end_idx - start_idx + 1; i++)
-// 	{
-// 		if (ridx == pos2)
-// 		{
-// 			*it = getValueAtIndex(left_list, lidx);
-// 			lidx++;
-// 		}
-// 		else if (lidx == pos1)
-// 		{
-// 			*it = getValueAtIndex(right_list, ridx);
-// 			ridx++;
-// 		}
-// 		else if (getValueAtIndex(right_list, ridx) > getValueAtIndex(left_list, lidx))
-// 		{
-// 			*it =  getValueAtIndex(left_list, lidx);
-// 			lidx++;
-// 		}
-// 		else
-// 		{
-// 			*it =  getValueAtIndex(right_list, ridx);
-// 			ridx++;
-// 		}
-// 		it++;
-// 	}
-// }
-
-// void	PmergeMe::insert_list(int start_idx, int mid_idx)
-// {
-// 	std::list<int>::iterator it = _list.begin();
-// 	// increase iterator to correct position
-// 	for (int i = 0; i < start_idx; i++)
-// 		++it;
-
-// 	std::list<int>::iterator start_it = it;
-// 	std::list<int>::iterator end_it = _list.begin();
-// 	// increase iterator to correct position
-// 	for (int i = 0; i < mid_idx; i++)
-// 		++end_it;
-
-// 	while (it != end_it) 
-// 	{
-// 		std::list<int>::iterator insertionPos = start_it;
-
-// 		while (insertionPos != it && *insertionPos <= *it)
-// 			++insertionPos;
-			
-// 		int tmp = *insertionPos;
-// 		*insertionPos = *it;
-// 		*it = tmp;
-		
-// 		it = insertionPos;
-// 		it++;
-		
-// 		// _list.insert(insertionPos, *it);
-// 		// it = _list.erase(it);
-// 	}
-// }
-
-// void	PmergeMe::sort_list(int start_idx, int end_idx)
-// {
-// 	if (end_idx - start_idx > _K)
-// 	{
-// 		int mid_idx = (start_idx + end_idx) / 2;
-// 		sort_list(start_idx, mid_idx);
-// 		sort_list(mid_idx + 1, end_idx);
-// 		merge_list(start_idx, mid_idx, end_idx);
-// 	}
-// 	else
-// 	{
-// 		insert_list(start_idx, end_idx);
-// 	}
-// 	std::cout << "after insert?:\t";
-// 	for (std::list<int>::iterator it = _list.begin(); it != _list.end(); ++it)
-// 		std::cout << *it << " ";
-// 	std::cout << std::endl;
-// }
-
-void	PmergeMe::merge_deque(int start_idx, int mid_idx, int end_idx)
+void	PmergeMe::merge_list(int start_idx, int mid_idx, int end_idx)
 {
-	int	pos1 = mid_idx - start_idx + 1;
-	int pos2 = end_idx - mid_idx;
-	std::deque<int> left_deque(_deque.begin() + start_idx, _deque.begin() + mid_idx + 1);
-	std::deque<int> right_deque(_deque.begin() + mid_idx + 1, _deque.begin() + end_idx + 1);
-	int ridx = 0;
-	int lidx = 0;
+	int	n1 = mid_idx - start_idx + 1;
+	int n2 = end_idx - mid_idx;
+	
+	// Create temporary sublists
+	std::list<int> LA, RA;
 
-	for (int i = start_idx; i < end_idx - start_idx + 1; i++)
+	// Copy data to temporary sublists LA[] and RA[]
+	for (int i = 0; i < n1; i++)
+		LA.push_back(getValueAtIndex(_list ,start_idx + i));
+	for (int j = 0; j < n2; j++)
+		RA.push_back(getValueAtIndex(_list, mid_idx + j + 1));
+
+	int RIDX = 0;
+	int LIDX = 0;
+	
+	std::list<int>::iterator	it = _list.begin();
+	for (int i = 0; i < start_idx; i++)
+		it++;
+
+	for (int i = start_idx; i <= end_idx; i++)
 	{
-		if (ridx == pos2)
+		if (RIDX == n2)
 		{
-			_deque[i] = left_deque[lidx];
-			lidx++;
+			*it = getValueAtIndex(LA, LIDX);
+			LIDX++;
 		}
-		else if (lidx == pos1)
+		else if (LIDX == n1)
 		{
-			_deque[i] = right_deque[ridx];
-			ridx++;
+			*it = getValueAtIndex(RA, RIDX);
+			RIDX++;
 		}
-		else if (right_deque[ridx] > left_deque[lidx])
+		else if (getValueAtIndex(RA, RIDX) > getValueAtIndex(LA, LIDX))
 		{
-			_deque[i] = left_deque[lidx];
-			lidx++;
+			*it =  getValueAtIndex(LA, LIDX);
+			LIDX++;
 		}
 		else
 		{
-			_deque[i] = right_deque[ridx];
-			ridx++;
+			*it =  getValueAtIndex(RA, RIDX);
+			RIDX++;
+		}
+		it++;
+	}
+}
+
+void	PmergeMe::insert_list(int start_idx, int mid_idx)
+{
+	std::list<int>::iterator it = _list.begin();
+	// increase iterator to correct position
+	for (int i = 0; i < start_idx; i++)
+		++it;
+
+	std::list<int>::iterator start_it = it;
+	std::list<int>::iterator end_it = _list.begin();
+	// increase iterator to correct position
+	for (int i = 0; i < mid_idx; i++)
+		++end_it;
+
+	while (it != end_it) 
+	{
+		std::list<int>::iterator insertionPos = start_it;
+
+		while (insertionPos != it && *insertionPos <= *it)
+			++insertionPos;
+			
+		int tmp = *insertionPos;
+		*insertionPos = *it;
+		*it = tmp;
+		
+		it = insertionPos;
+		it++;
+		
+		// _list.insert(insertionPos, *it);
+		// it = _list.erase(it);
+	}
+}
+
+void	PmergeMe::sort_list(int start_idx, int end_idx)
+{
+	if (end_idx - start_idx > _K)
+	{
+		int mid_idx = (start_idx + end_idx) / 2;
+		sort_list(start_idx, mid_idx);
+		sort_list(mid_idx + 1, end_idx);
+		merge_list(start_idx, mid_idx, end_idx);
+	}
+	else
+	{
+		insert_list(start_idx, end_idx + 1);
+	}
+	// std::cout << "after insert?:\t";
+	// for (std::list<int>::iterator it = _list.begin(); it != _list.end(); ++it)
+	// 	std::cout << *it << " ";
+	// std::cout << std::endl;
+}
+
+void	PmergeMe::merge_deque(int start_idx, int mid_idx, int end_idx)
+{
+	int n1 = mid_idx - start_idx + 1;
+	int n2 = end_idx - mid_idx;
+
+	// Create temporary subarrays
+	std::deque<int> LA, RA;
+
+	// Copy data to temporary subarrays LA[] and RA[]
+	for (int i = 0; i < n1; i++)
+		LA.push_back(_deque[start_idx + i]);
+	for (int j = 0; j < n2; j++)
+		RA.push_back(_deque[mid_idx + j + 1]);
+
+	int RIDX = 0;
+	int LIDX = 0;
+	for (int i = start_idx; i <= end_idx; i++) {
+		if (RIDX == n2) {
+			_deque[i] = LA[LIDX];
+			LIDX++;
+		} else if (LIDX == n1) {
+			_deque[i] = RA[RIDX];
+			RIDX++;
+		} else if (RA[RIDX] > LA[LIDX]) {
+			_deque[i] = LA[LIDX];
+			LIDX++;
+		} else {
+			_deque[i] = RA[RIDX];
+			RIDX++;
 		}
 	}
 }
@@ -205,7 +212,7 @@ void	PmergeMe::insert_deque(int start_idx, int mid_idx)
 		}
 		_deque[j] = tmp;
 	}
-	std::deque<int> tmpDeque(_deque.begin() + start_idx, _deque.begin() + mid_idx + 1);
+	// std::deque<int> tmpDeque(_deque.begin() + start_idx, _deque.begin() + mid_idx + 1);
 }
 
 void	PmergeMe::sort_deque(int start_idx, int end_idx)
@@ -219,6 +226,8 @@ void	PmergeMe::sort_deque(int start_idx, int end_idx)
 	}
 	else
 	{
+		// int tempres = end_idx - start_idx;
+		// std::cout << "(end_idx - start_idx): " << tempres << std::endl;
 		insert_deque(start_idx, end_idx);
 	}
 	// std::cout << "after insert?:\t";
@@ -226,9 +235,6 @@ void	PmergeMe::sort_deque(int start_idx, int end_idx)
 	// 	std::cout << *it << " ";
 	// std::cout << std::endl;
 }
-
-
-
 
 PmergeMe::PmergeMe(char *argv[])
 {
@@ -243,16 +249,16 @@ PmergeMe::PmergeMe(char *argv[])
 		_deque.push_back(static_cast<int>(val));
 	}
 
-	// std::cout << "Before:\t";
-	// for (std::list<int>::iterator it = _list.begin(); it != _list.end(); ++it)
-	// 	std::cout << *it << " ";
-	// std::cout << std::endl;
-	
-	
 	std::cout << "Before:\t";
-	for (std::deque<int>::iterator it = _deque.begin(); it != _deque.end(); ++it)
+	for (std::list<int>::iterator it = _list.begin(); it != _list.end(); ++it)
 		std::cout << *it << " ";
 	std::cout << std::endl;
+	
+	
+	// std::cout << "Before:\t";
+	// for (std::deque<int>::iterator it = _deque.begin(); it != _deque.end(); ++it)
+	// 	std::cout << *it << " ";
+	// std::cout << std::endl;
 	
 	// double elapsed = GetTimeStamp() - begin;
 	// std::cout << "Time measured: ";
@@ -261,17 +267,18 @@ PmergeMe::PmergeMe(char *argv[])
 	// uint64_t list_begin = micros();
 	
 	// uint64_t list_sort_time = micros() - list_begin;
-	// sort_list(0, _list.size() - 1);
-	// std::cout << "After:\t";
-	// for (std::list<int>::iterator it = _list.begin(); it != _list.end(); ++it)
-	// 	std::cout << *it << " ";
-	// std::cout << std::endl;
-	
-	sort_deque(0, _deque.size() - 1);
+	sort_list(0, _list.size() - 1);
 	std::cout << "After:\t";
-	for (std::deque<int>::iterator it = _deque.begin(); it != _deque.end(); ++it)
+	for (std::list<int>::iterator it = _list.begin(); it != _list.end(); ++it)
 		std::cout << *it << " ";
 	std::cout << std::endl;
+
+	// std::cout << "_deque.size(): " << _deque.size() << std::endl;
+	// sort_deque(0, _deque.size() - 1);
+	// std::cout << "After:\t";
+	// for (std::deque<int>::iterator it = _deque.begin(); it != _deque.end(); ++it)
+	// 	std::cout << *it << " ";
+	// std::cout << std::endl;
 	
 
 	// uint64_t deque_begin = micros();
